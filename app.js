0bd56534eaf6eae9d183413d3a8623b44ad007f2
ginsan1997 <96338131+ginsan1997@users.noreply.github.com>
2023-07-09 00:23:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+let point = document.querySelectorAll('.point')
+let imageSlider = document.querySelectorAll('.imageSlider')
+let leftBtn = document.getElementById('leftBtn')
+let rightBtn = document.getElementById('rightBtn')
+
+point[0].classList.add('activeImage')
+imageSlider[0].classList.add('activeImage')
+
+let counter = 0;
+
+for(let i=0; i<point.length; i++){
+    point[i].addEventListener('click',()=>{
+        for(let k = 0; k<imageSlider.length; k++){
+            point[k].classList.remove('activeImage')
+            imageSlider[k].classList.remove('activeImage')
+        }
+        counter = i;
+        imageSlider[counter].classList.add('activeImage');
+        point[counter].classList.add('activeImage');
+    })
+}
+
+leftBtn.addEventListener('click',()=>{
+    for(let k = 0; k<imageSlider.length; k++){
+        point[k].classList.remove('activeImage')
+        imageSlider[k].classList.remove('activeImage')
+    }
+    counter--
+    if (counter <0){
+        counter = imageSlider.length-1
+    }
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+})
+
+rightBtn.addEventListener('click',()=>{
+    for(let k = 0; k<imageSlider.length; k++){
+        point[k].classList.remove('activeImage')
+        imageSlider[k].classList.remove('activeImage')
+    }
+    counter++
+    if (counter >= imageSlider.length){
+        counter = 0
+    }
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+})
+
+
+function slowSlider () {
+    for(let k = 0; k<imageSlider.length; k++){
+        point[k].classList.remove('activeImage')
+        imageSlider[k].classList.remove('activeImage')
+    }
+    counter++
+    if (counter >= imageSlider.length){
+        counter = 0
+    }
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+}
+
+let second = 1000*2
+let TimerImage = setInterval(()=>slowSlider(), second)
+
+let blockSlider = document.getElementById('blockSlider')
+blockSlider.addEventListener('mouseover',()=>{
+    clearInterval(TimerImage)
+})
+
+blockSlider.addEventListener('mouseleave',()=>{
+    TimerImage = setInterval(()=>slowSlider(), second)
+})
